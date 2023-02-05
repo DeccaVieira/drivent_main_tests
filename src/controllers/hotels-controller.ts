d3b939fcdf69aa1cb8dab955abb827813eb721b1
Andreia Vieira <andreiavieira2353@gmail.com>
2023-02-05 16:10:03 -0300

    controller FindHotelById

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,7 +1,7 @@
-import { AuthenticatedRequest } from "@/middlewares";
-import { Response } from "express";
-import httpStatus from "http-status";
-import hotelService from "@/services/hotels-service";
+import { AuthenticatedRequest } from '@/middlewares';
+import { Response } from 'express';
+import httpStatus from 'http-status';
+import hotelService from '@/services/hotels-service';
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
@@ -11,10 +11,28 @@ export async function getHotels(req: AuthenticatedRequest, res: Response) {
 
     return res.status(200).send(hotels);
   } catch (error) {
-    if (error.name === "NotFoundError") {
+    if (error.name === 'NotFoundError') {
       return res.status(httpStatus.NOT_FOUND);
     }
-    if (error.name === "PaymentRequiredError") {
+    if (error.name === 'PaymentRequiredError') {
+      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    }
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
+
+export async function findHotelsById(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const hotelId = Number(req.params.hotelId);
+
+  try {
+    const hotels = await hotelService.findHotelsById(userId, hotelId);
+    return res.status(httpStatus.OK).send(hotels);
+  } catch (error) {
+    if (error.name === 'NotFoundError') {
+      return res.status(httpStatus.NOT_FOUND);
+    }
+    if (error.name === 'PaymentRequiredError') {
       return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
     }
     return res.sendStatus(httpStatus.BAD_REQUEST);
