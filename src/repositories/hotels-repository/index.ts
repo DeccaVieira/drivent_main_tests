448f7f9191dab9bbf49120f83667abf0c5f75a55
Andreia Vieira <andreiavieira2353@gmail.com>
2023-02-05 16:21:08 -0300

    Repository FindHotelById

--- src/repositories/hotels-repository/index.ts
+++ src/repositories/hotels-repository/index.ts
@@ -4,3 +4,29 @@ import { Hotel, Room, TicketType } from "@prisma/client";
 async function findOptionHotels() {
   return prisma.hotel.findMany({});
 }
+
+async function findOptionHotelsById(hotelId: number) {
+  const hotels = await prisma.hotel.findFirst({
+    where: { id: hotelId },
+    include: {
+      Rooms: {
+        where: {
+          hotelId
+        },
+        select: {
+          id: true,
+          name: true,
+          capacity: true,
+          hotelId: true,
+          createdAt: true,
+          updatedAt: true
+        }
+      }
+    },
+  });
+  return hotels;
+}
+
+const hotelRepository = { findOptionHotels, findOptionHotelsById };
+
+export default hotelRepository;
